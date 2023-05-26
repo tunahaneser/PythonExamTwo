764b9707215aeebd47c62f89b715242a9c2d9550
Tunahan Eser <tuna-1905@hotmail.com>
2023-05-26 16:50:42 +0300

    Create ExamTwo.py

---
+++ ExamTwo.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+color = input("You are fovorite color? ")
+print(name + " aramıza hoşgeldin, yaşına uygun bölümü seçerek aramıza katıl. Senin yaşın: " +age + " Senin en sevdiğin renk: " +color)
